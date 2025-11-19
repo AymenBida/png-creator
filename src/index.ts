a7e7f20a84c8550dc455a5e97d21b673cce684aa
Aymen Bida <aymen.bida.00@gmail.com>
2025-11-18 19:37:23 -0500

    seperate exports

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { getPNGCreator } from './png.js';
-export type { PNGCreatorDependencies } from './png.js';
-export { PixelCanvas } from './pixelCanvas.js';
-export type { PixelCanvasOptions, IPixelCanvas } from './pixelCanvas.js';
